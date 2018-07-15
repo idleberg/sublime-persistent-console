b952add33f12fff40f2d330c617cf0b56417845c
idleberg <jan@idleberg.com>
2018-07-15 22:13:51 +0200

    refactor

--- Persistent Console.py
+++ Persistent Console.py
@@ -14,30 +14,12 @@ class PersistentConsoleToggleCommand(WindowCommand):
         """Show Console for new windows."""
         settings = sublime.load_settings("Persistent Console.sublime-settings")
         enablePackage = settings.get("enable")
+        verb = "disabled" if enablePackage is True else "enabled"
 
-        if enablePackage is False:
-            settings.set("enable", True)
-            self.window.status_message("Persistent Console enabled")
-        else:
-            settings.set("enable", False)
-            self.window.status_message("Persistent Console disabled")
-
+        settings.set("enable", not enablePackage)
         sublime.save_settings("Persistent Console.sublime-settings")
 
-class ConditionalShowConsoleCommand(WindowCommand):
-    """Checks whether to show console on new window."""
-
-    def run(self):
-        """Show Console for new windows."""
-        enablePackage = sublime.load_settings("Persistent Console.sublime-settings").get("enable")
-
-        if enablePackage is False:
-            return
-
-        activePanels = self.window.active_panel()
-
-        if activePanels is None or "console" not in activePanels:
-            self.window.run_command("show_console")
+        self.window.status_message("Persistent Console {}".format(verb))
 
 class ShowConsoleCommand(WindowCommand):
     """Adds Show Console Command to Command Palette."""
@@ -46,9 +28,12 @@ class ShowConsoleCommand(WindowCommand):
         """Show Console."""
         self.window.run_command("show_panel", {"panel": "console"})
 
-class ConditionalShowConsoleListener(EventListener):
+class ShowConsoleListener(EventListener):
     """Adds listener events."""
 
     def on_new_async(self, view):
         """Listen for new windows."""
-        view.window().run_command("conditional_show_console")
+        enablePackage = sublime.load_settings("Persistent Console.sublime-settings").get("enable")
+
+        if enablePackage is True:
+           view.window().run_command("show_console")
