cc625fa3ac56e171896a3c502cb768907e342f3e
idleberg <jan@idleberg.com>
2018-07-15 18:05:32 +0200

    first commit

---
+++ Persistent Console.py
@@ -0,0 +1,54 @@
+"""
+This borrows heavily from the BinaryPlist package.
+
+https://github.com/idleberg/sublime-persistent-console
+"""
+
+import sublime
+from sublime_plugin import EventListener, WindowCommand
+
+class PersistentConsoleToggleCommand(WindowCommand):
+    """Checks whether to show console on new window."""
+
+    def run(self):
+        """Show Console for new windows."""
+        settings = sublime.load_settings("Persistent Console.sublime-settings") or True
+        enablePackage = settings.get("enable")
+
+        if enablePackage is False:
+            settings.set("enable", True)
+            self.window.status_message("Persistent Console enabled")
+        else:
+            settings.set("enable", False)
+            self.window.status_message("Persistent Console disabled")
+
+        sublime.save_settings("Persistent Console.sublime-settings")
+
+class ConditionalShowConsoleCommand(WindowCommand):
+    """Checks whether to show console on new window."""
+
+    def run(self):
+        """Show Console for new windows."""
+        enablePackage = sublime.load_settings("Persistent Console.sublime-settings").get("enable")
+
+        if enablePackage is False:
+            return
+
+        activePanels = self.window.active_panel()
+
+        if activePanels is None or "console" not in activePanels:
+            self.window.run_command('show_console')
+
+class ShowConsoleCommand(WindowCommand):
+    """Adds Show Console Command to Command Palette."""
+
+    def run(self):
+        """Show Console."""
+        self.window.run_command("show_panel", {"panel": "console"})
+
+class ConditionalShowConsoleListener(EventListener):
+    """Adds listener events."""
+
+    def on_new(self, view):
+        """Listen for new windows."""
+        view.window().run_command('conditional_show_console')
---
+++ README.md
@@ -0,0 +1,34 @@
+# sublime-auto-open-console
+
+[![The MIT License](https://img.shields.io/badge/license-MIT-orange.svg?style=flat-square)](http://opensource.org/licenses/MIT)
+[![Package Control](https://packagecontrol.herokuapp.com/downloads/Auto%20Open%20console.svg?style=flat-square)](https://packagecontrol.io/packages/Auto%20Open%20console)
+[![GitHub](https://img.shields.io/github/release/idleberg/sublime-auto-open-console.svg?style=flat-square)](https://github.com/idleberg/sublime-auto-open-console/releases)
+[![Travis](https://img.shields.io/travis/idleberg/sublime-auto-open-console.svg?style=flat-square)](https://travis-ci.org/idleberg/sublime-auto-open-console)
+
+Package that opens console whenever a new window or tab is opened. Useful for package developers.
+
+## Installation
+
+### Package Control
+
+1. Make sure you already have [Package Control](https://packagecontrol.io/) installed
+2. Choose *“Install Package”* from the Command Palette (<kbd>Super</kbd>+<kbd>Shift</kbd>+<kbd>p</kbd>)
+3. Type *“Auto Open Console”* and press <kbd>Enter</kbd>
+
+### Using Git
+
+1. Change to your Sublime Text `Packages` directory
+2. Clone repository `git clone https://github.com/idleberg/sublime-auto-open-console.git 'Auto Open Console'`
+
+### Manual installation
+
+1. Download the latest [stable release](https://github.com/idleberg/sublime-auto-open-console/releases)
+2. Unzip the archive to your Sublime Text `Packages` directory
+
+## License
+
+This work is licensed under the [The MIT License](LICENSE).
+
+## Donate
+
+You are welcome support this project using [Flattr](https://flattr.com/submit/auto?user_id=idleberg&url=https://github.com/idleberg/sublime-auto-open-console) or Bitcoin `17CXJuPsmhuTzFV2k4RKYwpEHVjskJktRd`
